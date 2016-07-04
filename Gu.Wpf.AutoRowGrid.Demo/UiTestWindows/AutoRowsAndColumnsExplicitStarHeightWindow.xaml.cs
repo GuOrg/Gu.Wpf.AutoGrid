ffe24501a255157c1b768ce0420fb44345293e9e
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-04 17:03:45 +0200

    Test Star Height.

---
+++ Gu.Wpf.AutoRowGrid.Demo/UiTestWindows/AutoRowsAndColumnsExplicitStarHeightWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Gu.Wpf.AutoRowGrid.Demo.UiTestWindows
+{
+    using System.Windows;
+
+    public partial class AutoRowsAndColumnsExplicitStarHeightWindow : Window
+    {
+        public AutoRowsAndColumnsExplicitStarHeightWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Gu.Wpf.AutoRowGrid.UiTests/AutoRowsAndColumnsExplicitStarHeightTests.cs
@@ -0,0 +1,18 @@
+namespace Gu.Wpf.AutoRowGrid.UiTests
+{
+    using NUnit.Framework;
+
+    public class AutoRowsAndColumnsExplicitStarHeightTests : WindowTest
+    {
+        public override string Title { get; } = "AutoRowsAndColumnsExplicitStarHeightWindow";
+
+        [Test]
+        public void Bounds()
+        {
+            Assert.AreEqual("0,0 35,100", this.Window.GetItemPosition("R0C0"));
+            Assert.AreEqual("35,0 150,100", this.Window.GetItemPosition("R0C1"));
+            Assert.AreEqual("0,100 35,200", this.Window.GetItemPosition("R1C0"));
+            Assert.AreEqual("35,100 150,200", this.Window.GetItemPosition("R1C1"));
+        }
+    }
+}
