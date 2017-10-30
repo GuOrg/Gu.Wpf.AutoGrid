14995fd215839ccdbe9bd4b8f0464988e673f74f
JohanLarsson <johan_gavle1@hotmail.com>
2017-10-30 13:02:35 +0100

    Update paket & dependencies.

--- Gu.Wpf.AutoRowGrid.UiTests/Helpers/Info.cs
+++ Gu.Wpf.AutoRowGrid.UiTests/Helpers/Info.cs
@@ -1,24 +1,7 @@
 ﻿namespace Gu.Wpf.AutoRowGrid.UiTests
 {
-    using System;
-    using System.IO;
-    using System.Reflection;
-
     public static class Info
     {
-        public static string ExeFileName { get; } = GetExeFileName();
-
-        private static string GetExeFileName()
-        {
-            var assembly = Assembly.GetExecutingAssembly();
-            var testDirestory = Path.GetDirectoryName(new Uri(assembly.CodeBase).AbsolutePath);
-            var assemblyName = assembly.GetName().Name;
-            var exeDirectoryName = assemblyName.Replace("UiTests", "Demo");
-            // ReSharper disable once PossibleNullReferenceException
-            var exeDirectory = testDirestory.Replace(assemblyName, exeDirectoryName);
-            var fileName = Path.Combine(exeDirectory, "Gu.Wpf.AutoRowGrid.Demo.exe");
-
-            return fileName;
-        }
+        public static string ExeFileName { get; } = Gu.Wpf.UiAutomation.Application.FindExe("Gu.Wpf.AutoRowGrid.Demo.exe");
     }
 }
