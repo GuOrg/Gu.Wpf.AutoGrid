c6a5e59cca128f794ae8796fa693d8fd95f8c1ce
JohanLarsson <johan_gavle1@hotmail.com>
2018-01-03 11:01:15 +0100

    Update dependencies.

--- Gu.Wpf.AutoRowGrid.UiTests/Helpers/WindowExt.cs
+++ Gu.Wpf.AutoRowGrid.UiTests/Helpers/WindowExt.cs
@@ -1,4 +1,4 @@
-﻿namespace Gu.Wpf.AutoRowGrid.UiTests
+namespace Gu.Wpf.AutoRowGrid.UiTests
 {
     using System.Globalization;
     using Gu.Wpf.UiAutomation;
@@ -8,7 +8,7 @@
         public static string GetItemPosition(this Window window, string name)
         {
             var groupBox = window.FindGroupBox();
-            var item = groupBox.FindFirstDescendant(x => x.ByAutomationId(name));
+            var item = groupBox.FindFirstDescendant(Conditions.ByAutomationId(name));
             var topLeft = item.Bounds.TopLeft - groupBox.Bounds.TopLeft;
             var bottomRight = item.Bounds.BottomRight - groupBox.Bounds.TopLeft;
             return $"{topLeft.ToString(CultureInfo.InvariantCulture)} {bottomRight.ToString(CultureInfo.InvariantCulture)}";
