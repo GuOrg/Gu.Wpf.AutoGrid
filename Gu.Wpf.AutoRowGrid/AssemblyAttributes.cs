b2a89575dd197667d83ef7840baf13ae855a1b3e
JohanLarsson <johan_gavle1@hotmail.com>
2020-02-10 09:51:09 +0100

    Generate assembly info.

---
+++ Gu.Wpf.AutoRowGrid/AssemblyAttributes.cs
@@ -0,0 +1,6 @@
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsDefinition("http://gu.se/AutoRowGrid", "Gu.Wpf.AutoRowGrid")]
+[assembly: XmlnsPrefix("http://gu.se/AutoRowGrid", "autoRowGrid")]
