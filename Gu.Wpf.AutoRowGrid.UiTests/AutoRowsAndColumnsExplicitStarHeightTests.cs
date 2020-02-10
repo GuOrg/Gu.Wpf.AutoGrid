23b2e0b521bbf650d030a79e83b816f4dcb42727
JohanLarsson <johan_gavle1@hotmail.com>
2020-02-10 10:01:08 +0100

    Simplify: using declarations

--- Gu.Wpf.AutoRowGrid.UiTests/AutoRowsAndColumnsExplicitStarHeightTests.cs
+++ Gu.Wpf.AutoRowGrid.UiTests/AutoRowsAndColumnsExplicitStarHeightTests.cs
@@ -10,14 +10,12 @@ namespace Gu.Wpf.AutoRowGrid.UiTests
         [Test]
         public void Bounds()
         {
-            using (var app = Application.Launch("Gu.Wpf.AutoRowGrid.Demo.exe", WindowName))
-            {
-                var window = app.MainWindow;
-                Assert.AreEqual("0,0 35,100", window.GetItemPosition("R0C0"));
-                Assert.AreEqual("35,0 150,100", window.GetItemPosition("R0C1"));
-                Assert.AreEqual("0,100 35,200", window.GetItemPosition("R1C0"));
-                Assert.AreEqual("35,100 150,200", window.GetItemPosition("R1C1"));
-            }
+            using var app = Application.Launch("Gu.Wpf.AutoRowGrid.Demo.exe", WindowName);
+            var window = app.MainWindow;
+            Assert.AreEqual("0,0 35,100", window.GetItemPosition("R0C0"));
+            Assert.AreEqual("35,0 150,100", window.GetItemPosition("R0C1"));
+            Assert.AreEqual("0,100 35,200", window.GetItemPosition("R1C0"));
+            Assert.AreEqual("35,100 150,200", window.GetItemPosition("R1C1"));
         }
     }
 }
--- Gu.Wpf.AutoRowGrid.UiTests/NestedRowsExplicitHeightTests.cs
+++ Gu.Wpf.AutoRowGrid.UiTests/NestedRowsExplicitHeightTests.cs
@@ -10,18 +10,16 @@ namespace Gu.Wpf.AutoRowGrid.UiTests
         [Test]
         public void Bounds()
         {
-            using (var app = Application.Launch("Gu.Wpf.AutoRowGrid.Demo.exe", WindowName))
-            {
-                var window = app.MainWindow;
-                Assert.AreEqual("0,0 35,25", window.GetItemPosition("R0C0"));
-                Assert.AreEqual("35,0 150,25", window.GetItemPosition("R0C1"));
-                Assert.AreEqual("0,25 35,50", window.GetItemPosition("R1C0"));
-                Assert.AreEqual("35,25 150,50", window.GetItemPosition("R1C1"));
-                Assert.AreEqual("0,50 35,80", window.GetItemPosition("R2C0"));
-                Assert.AreEqual("35,50 150,80", window.GetItemPosition("R2C1"));
-                Assert.AreEqual("0,80 35,110", window.GetItemPosition("R3C0"));
-                Assert.AreEqual("35,80 150,110", window.GetItemPosition("R3C1"));
-            }
+            using var app = Application.Launch("Gu.Wpf.AutoRowGrid.Demo.exe", WindowName);
+            var window = app.MainWindow;
+            Assert.AreEqual("0,0 35,25", window.GetItemPosition("R0C0"));
+            Assert.AreEqual("35,0 150,25", window.GetItemPosition("R0C1"));
+            Assert.AreEqual("0,25 35,50", window.GetItemPosition("R1C0"));
+            Assert.AreEqual("35,25 150,50", window.GetItemPosition("R1C1"));
+            Assert.AreEqual("0,50 35,80", window.GetItemPosition("R2C0"));
+            Assert.AreEqual("35,50 150,80", window.GetItemPosition("R2C1"));
+            Assert.AreEqual("0,80 35,110", window.GetItemPosition("R3C0"));
+            Assert.AreEqual("35,80 150,110", window.GetItemPosition("R3C1"));
         }
     }
 }
