c9cca1ebb3fb7e88f57460fe6461604096b74f9c
Johan Larsson <johan_gavle1@hotmail.com>
2021-01-02 01:40:14 +0100

    [assembly: CLSCompliant(false)]

--- Gu.Wpf.AutoRowGrid/AssemblyAttributes.cs
+++ Gu.Wpf.AutoRowGrid/AssemblyAttributes.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Windows;
 using System.Windows.Markup;
 
+[assembly: CLSCompliant(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: XmlnsDefinition("http://gu.se/AutoRowGrid", "Gu.Wpf.AutoRowGrid")]
 [assembly: XmlnsPrefix("http://gu.se/AutoRowGrid", "autoRowGrid")]
